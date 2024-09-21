08ecbf041ed9818d2a37b55cea85785fad9ca688
Friendly Ware <friendlywaresmp@gmail.com>
2024-09-21 16:13:31 +0200

    fix: fix gradient bar not rendering as intended

--- src/components/GradientBar.vue
+++ src/components/GradientBar.vue
@@ -3,18 +3,23 @@
 </script>
 
 <template>
-<div class="gradient-bar">
-
-</div>
+  <div class="wrapper">
+    <div class="gradient-bar"></div>
+  </div>
 </template>
 
 <style scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+}
+
 .gradient-bar {
   position: fixed;
   bottom: 10px;
-  background: linear-gradient(to bottom right, #ff00f2, #7700ff, #5300ff);
-  width: 99vw;
+  width: 97vw;
   height: 3px;
+  background: linear-gradient(to bottom right, #ff00f2, #7700ff, #5300ff);
   border-radius: 30px;
 }
 </style>
