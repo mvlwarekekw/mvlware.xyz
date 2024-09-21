1ff202fefb45af80fdd54259a849096f0c406be2
Friendly Ware <friendlywaresmp@gmail.com>
2024-09-21 12:17:23 +0200

    feat: add Hero component for home (mvlware.xyz/)

---
+++ src/components/Hero.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<section>
+  <div class="hero">
+    <div class="title">
+      <h1 class="text-xxxl">Heya,<br>I'm mvlware</h1>
+    </div>
+    <div class="seal">
+      <img src="/src/assets/seal.png" alt="cute seal" class="seal-img">
+    </div>
+  </div>
+</section>
+</template>
+
+<style scoped>
+.hero {
+  display: grid;
+  grid-template-columns: 65% 35%;
+  width: 100%
+}
+
+.title {
+  height: 80vh;
+  padding-left: 100px;
+  align-content: center;
+}
+
+.seal {
+  display: flex;
+  height: 80vh;
+  align-items: center;
+  justify-content: center;
+
+}
+
+.seal-img {
+  height: 50vh;
+  width: 50vh;
+  border-radius: 100%;
+
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 
 import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
 </script>
 
 <template>
   <div class="container">
     <Navbar />
+    <Hero />
   </div>
 </template>
