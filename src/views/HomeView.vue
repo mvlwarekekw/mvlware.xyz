5d2e4eb0024bf8f5bbcde847bd4acd2943a38244
Friendly Ware <friendlywaresmp@gmail.com>
2024-09-21 16:02:22 +0200

    remove: remove unused imports

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 
-import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
-import GradientBar from "@/components/GradientBar.vue";
 </script>
 
 <template>
