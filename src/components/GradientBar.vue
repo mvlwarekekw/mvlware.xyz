923a422cad92f4bea0c69e90c6e69d419bfae6cc
Friendly Ware <friendlywaresmp@gmail.com>
2024-09-21 12:38:56 +0200

    feat: add gradient bar to bottom of  the screen

---
+++ src/components/GradientBar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="gradient-bar">
+
+</div>
+</template>
+
+<style scoped>
+.gradient-bar {
+  position: fixed;
+  bottom: 10px;
+  background: linear-gradient(#ff00f2, #5300ff);
+  width: 99vw;
+  height: 5px;
+  border-radius: 20px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,13 @@
 
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
+import GradientBar from "@/components/GradientBar.vue";
 </script>
 
 <template>
   <div class="container">
     <Navbar />
     <Hero />
+    <GradientBar />
   </div>
 </template>
