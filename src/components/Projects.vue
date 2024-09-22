6e328677b2887551ce9258e88916410c8bbc1f95
Friendly Ware <friendlywaresmp@gmail.com>
2024-09-22 20:02:34 +0200

    feat: added project page + view

---
+++ src/components/Projects.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+import Project from "@/components/Project.vue";
+</script>
+
+<template>
+  <div class="container-sm">
+    <div class="flex h-flex-center title">
+      <h1 class="text-xxl">Projects</h1>
+    </div>
+    <div class="projects">
+      <Project
+          name="IskraMC"
+          description="IskraMC is a minecraft minigame server network currently in development."
+          :technologies="['/src/assets/kotlin.svg', '/src/assets/mongodb.svg']"
+      />
+      <Project
+          name="Personal Website"
+          description="My personal Website"
+          link="https://mvlware.xyz"
+          display_link="mvlware.xyz"
+          :technologies="['/src/assets/vuejs.png']"
+      />
+      <Project
+          name="Coming Soon"
+          description="I'll be working on more projects soon, which will be added here."
+          :technologies="[]"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  margin: 50px 0;
+}
+
+.projects {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+</style>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Projects from "@/components/Projects.vue";
+
+</script>
+
+<template>
+<Projects />
+</template>
+
+<style scoped>
+
+</style>
