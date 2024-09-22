63a3d99e7c45d02f77fc654b98cc6a380844754e
Friendly Ware <friendlywaresmp@gmail.com>
2024-09-22 14:17:30 +0200

    feat: add not found page

---
+++ src/components/NotFound.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="wrapper">
+  <h1 class="text-xxxl">404 Not Found</h1>
+  <img src="/src/assets/confused_seal.png" alt="" class="seal">
+</div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+}
+
+.seal {
+  border-radius: 100%;
+}
+</style>
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import NotFound from "@/components/NotFound.vue";
+</script>
+
+<template>
+<NotFound />
+</template>
+
+<style scoped>
+
+</style>
