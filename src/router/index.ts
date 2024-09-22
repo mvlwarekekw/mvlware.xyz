585453acc27886c029e4250abf950eae7006e6e6
Friendly Ware <friendlywaresmp@gmail.com>
2024-09-22 20:02:44 +0200

    feat: add projects to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from "@/views/AboutView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
+import ProjectsView from "@/views/ProjectsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: ProjectsView
     },
     {
       path: '/:catchAll(.*)',
