07009501499498db607c8d27c01b1193052b6146
Friendly Ware <friendlywaresmp@gmail.com>
2024-10-03 17:41:49 +0200

    feat: add vercel speed insights

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@ import { RouterView } from 'vue-router'
 import Navbar from "@/components/Navbar.vue";
 import GradientBar from "@/components/GradientBar.vue";
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 
 inject()
+injectSpeedInsights()
 </script>
 
 <template>
