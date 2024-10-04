ef1ca7786ca9ca25cbab30233db8bd6cce6ed101
Friendly Ware <friendlywaresmp@gmail.com>
2024-10-04 15:13:00 +0200

    fix: fix wrong classname in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -115,15 +115,15 @@ function toggle() {
     top: 6px;
   }
 
-  .hamburger.active .bar:nth-child(2) {
+  .burger.active .bar:nth-child(2) {
     opacity: 0;
   }
 
-  .hamburger.active .bar:nth-child(1) {
+  .burger.active .bar:nth-child(1) {
     transform: translateY(8px) rotate(45deg);
   }
 
-  .hamburger.active .bar:nth-child(3) {
+  .burger.active .bar:nth-child(3) {
     transform: translateY(-8px) rotate(-45deg);
   }
 
